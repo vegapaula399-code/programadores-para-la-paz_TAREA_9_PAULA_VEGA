699834f7ee3ac4349678797c27c2d8df475b553f
vegapaula399-code <vegapaula399@gmail.com>
2026-07-02 16:10:09 +0000

    Actividad semana 9

---
+++ semana9/server.js
@@ -0,0 +1,37 @@
+http://localhost:3000
+http://localhost:3000/estado
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo y funcionando');
+});
+
+app.get('/estado', (req, res) => {
+  res.json({
+    estado: "Servidor funcionando",
+    servicio: "API comunitaria"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo y funcionando');
+});
+
+app.get('/estado', (req, res) => {
+  res.json({
+    estado: "Servidor funcionando",
+    servicio: "API comunitaria"
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
